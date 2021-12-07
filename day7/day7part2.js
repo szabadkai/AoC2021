eb6b5a2a2cbda57cd08e2e36e5d93f781c65ac30
Levente Szabadkai <levente.szabadkai@logmein.com>
2021-12-07 06:36:19 +0100

    day7

---
+++ day7/day7part2.js
@@ -0,0 +1,26 @@
+import { readFile } from "fs";
+
+function readInput(input) {
+    return input.split(",").map((x) => parseInt(x, 10));
+}
+
+readFile("./data.txt", "utf8", (err, data) => {
+    const input = readInput(data);
+    const minX = Math.min(...input);
+    const maxX = Math.max(...input);
+    console.log(`min: ${minX}, ${maxX}`);
+    let minFuel = Number.POSITIVE_INFINITY;
+    let minPlace = 0;
+    for (let i = minX; i <= maxX; i++) {
+        let sum = 0;
+        for (let fish of input) {
+            // console.log(i, fish, Math.abs(fish - i));
+            sum += (Math.abs(fish - i) * (Math.abs(fish - i) + 1)) / 2;
+        }
+        if (sum < minFuel) {
+            minFuel = sum;
+            minPlace = i;
+        }
+    }
+    console.log("Found new min fuel: ", minFuel, minPlace);
+});
